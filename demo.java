28beab0156c6230e84f79e2139fcfc2e1435f92e
junaid khan <mdjunaidkhan1206@gmail.com>
2022-01-21 16:12:05 +0530

    ij

--- demo.java
+++ demo.java
@@ -5,5 +5,9 @@ class demo {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello junaid");
+        int src=34;
+        final  int FIN=90;
+        System.out.println("the number is "+src);
     }
 }
+
