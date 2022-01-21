968631f3412400ec8f85757fca3dc1917fdd54d4
junaid khan <mdjunaidkhan1206@gmail.com>
2022-01-21 16:31:59 +0530

    hello world added

--- demo.java
+++ demo.java
@@ -5,9 +5,9 @@ class demo {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello junaid");
-        int src=34;
+        int sr=34;
         final  int FIN=90;
-        System.out.println("the number is "+src);
+        System.out.println("the number is "+sr);
     }
 }
 
