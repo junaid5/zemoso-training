ba58cede7a8d3ba257c5ae189fa0bbee18efcd91
junaid khan <mdjunaidkhan1206@gmail.com>
2022-01-21 16:29:25 +0530

    conversion file added

---
+++ ConversionFun.java
@@ -0,0 +1,12 @@
+package intro;
+
+public class ConversionFun {
+    float f=(float) 5.6;
+    byte b=4;
+    short s=56;
+    int i=689;
+    long l=983023;
+    double d=7.8908;
+    char c=8;
+    boolean bo=false;
+}
