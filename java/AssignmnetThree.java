cba02fd9bdf705822390dd81f2bf9d33aaebe303
junaid khan <mdjunaidkhan1206@gmail.com>
2022-01-27 12:28:05 +0530

    java assignment 3

---
+++ java/AssignmnetThree.java
@@ -0,0 +1,22 @@
+package intro.java;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class AssignmnetThree {
+    public static void main(String[] args) throws IOException {
+        long start=System.currentTimeMillis();
+        String ipAddress="127.0.0.1";
+        InetAddress giveAddress=InetAddress.getByName(ipAddress);
+        System.out.println("sending ping request to:"+ipAddress);
+        if(giveAddress.isReachable(1000)){
+            System.out.println("host is reachable");
+        }
+        else{
+            System.out.println("host is not reachable");
+        }
+        long end=System.currentTimeMillis();
+        long time=(end-start)<<1;
+        System.out.println("the median time is:"+time);
+    }
+}
